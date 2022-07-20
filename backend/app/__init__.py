0f57ec01e4e74334a1a50ffc325d361339686d99
Brandon Flores <brandonflores647@gmail.com>
2022-07-19 21:08:51 -0400

    initialized flask with application

---
+++ backend/app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_migrate import Migrate
+
+from .config import Config
+from .models import db
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+migrate = Migrate(app, db)
