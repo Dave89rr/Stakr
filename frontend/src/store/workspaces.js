38cfab8f72121397c2bfe74fe9420b5ffb067a87
Justin-Stockton <justinstockton67@gmail.com>
2022-07-20 13:44:11 -0400

    made types for workspace store

---
+++ frontend/src/store/workspaces.js
@@ -0,0 +1,14 @@
+// ==== Types ====//
+
+const CREATE_WORKSPACE = "workspace/CREATE_WORKSPACE";
+
+const GET_WORKSPACE = "workspace/GET_WORKSPACE";
+
+const UPDATE_WORKSPACE = "workspace/UPDATE_WORKSPACE";
+
+const DELETE_WORKSPACE = "workspace/DELETE_WORKSPACE";
+
+// ==== Actions ====//
+
+// ==== Thunks ====//
+// ==== Reducers ====//
