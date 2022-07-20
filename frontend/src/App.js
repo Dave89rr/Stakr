fd22866dc539df51d7f540968d9bab67981132cd
David Rivera <daleriro@gmail.com>
2022-07-20 12:47:44 -0400

    fonts, basic router, dummy homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,18 @@
+import { Route, Switch, Redirect } from 'react-router-dom';
+import HomePage from './components/Pages/Homepage';
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />;
+        </Route>
+        <Route>
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
---
+++ frontend/src/components/Pages/Homepage/Homepage.js
@@ -0,0 +1,7 @@
+import classes from './Homepage.module.css';
+
+function HomePage() {
+  return <h1 className={classes.title}>Howdy</h1>;
+}
+
+export default HomePage;
