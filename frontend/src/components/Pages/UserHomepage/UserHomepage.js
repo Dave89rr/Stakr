680a9a6122b82a55e6e0a1da361b2b55b409830b
David Rivera <daleriro@gmail.com>
2022-07-20 18:14:27 -0400

    updated widths in worspace cluster

--- frontend/src/components/Pages/UserHomepage/UserHomepage.js
+++ frontend/src/components/Pages/UserHomepage/UserHomepage.js
@@ -1,13 +1,18 @@
 import classes from './UserHomepage.module.css';
 import uniCss from '../pagesuniversal.module.css';
-import BoardCard from '../../Elements/BoardCard';
 import WorkspaceCluster from '../../Elements/WorkspaceCluster';
 
 function UserHomepage() {
   return (
     <div className={uniCss.mainContainer}>
-      <h3>Your Workspaces</h3>
-      <WorkspaceCluster />
+      <div className={classes.container}>
+        <div className={classes.workspacesMenu}></div>
+        <div className={classes.workspacesView}>
+          <h3>Your Workspaces</h3>
+          <WorkspaceCluster />
+          <WorkspaceCluster />
+        </div>
+      </div>
     </div>
   );
 }
