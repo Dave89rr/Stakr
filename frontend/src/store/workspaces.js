dcb12de62ced6f063a660ad0eb0ef33275b650cb
Justin-Stockton <justinstockton67@gmail.com>
2022-07-20 13:56:46 -0400

    made thunk for create WS

--- frontend/src/store/workspaces.js
+++ frontend/src/store/workspaces.js
@@ -9,28 +9,28 @@ const UPDATE_WORKSPACE = "workspace/UPDATE_WORKSPACE";
 const DELETE_WORKSPACE = "workspace/DELETE_WORKSPACE";
 
 // ==== Actions ====//
-const actionCreateWS = (workspace) => {
+const actionCreateWorkspace = (workspace) => {
   return {
     type: CREATE_WORKSPACE,
     workspace,
   };
 };
 
-const actionGetWS = (workspace) => {
+const actionGetWorkspace = (workspace) => {
   return {
     type: GET_WORKSPACE,
     workspace,
   };
 };
 
-const actionUpdateWS = (workspace) => {
+const actionUpdateWorkspace = (workspace) => {
   return {
     type: UPDATE_WORKSPACE,
     workspace,
   };
 };
 
-const actionDeleteWS = (workspace) => {
+const actionDeleteWorkspace = (workspace) => {
   return {
     type: DELETE_WORKSPACE,
     workspace,
@@ -38,4 +38,19 @@ const actionDeleteWS = (workspace) => {
 };
 
 // ==== Thunks ====//
+
+export const thunkCreateworkspace = (workspace) => async (dispatch) => {
+  const response = await fetch(`/api/w/create`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(workspace),
+  });
+
+  if (response.ok) {
+    const workspace = await response.json();
+    dispatch(actionCreateWorkspace(workspace.workspace));
+  }
+};
 // ==== Reducers ====//
