c34abb020adcf349a996ec8f65eaaf84997fafcf
Brandon Flores <brandonflores647@gmail.com>
2022-07-21 14:02:05 -0400

    userAuth setup complete

--- frontend/src/components/Pages/SignUpPage/SignUpPage.js
+++ frontend/src/components/Pages/SignUpPage/SignUpPage.js
@@ -1,12 +1,97 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { Redirect } from 'react-router-dom';
+import { signUp } from '../../../store/session';
+
 import classes from './SignUpPage.module.css';
 import uniCss from '../pagesuniversal.module.css';
 
-function SignUpPage() {
+const SignUpPage = () => {
+  const [errors, setErrors] = useState([]);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
+  const user = useSelector(state => state.session.user);
+  const dispatch = useDispatch();
+
+  const onSignUp = async (e) => {
+    e.preventDefault();
+    if (password === repeatPassword) {
+      const data = await dispatch(signUp(username, email, password));
+      if (data) {
+        setErrors(data)
+      }
+    }
+  };
+
+  const updateUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const updateEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const updatePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const updateRepeatPassword = (e) => {
+    setRepeatPassword(e.target.value);
+  };
+
+  if (user) {
+    return <Redirect to='/' />;
+  }
+
   return (
-    <div className={uniCss.mainContainer}>
-      <h1>SignUpPage</h1>;
-    </div>
+    <form onSubmit={onSignUp}>
+      <div>
+        {errors.map((error, ind) => (
+          <div key={ind}>{error}</div>
+        ))}
+      </div>
+      <div>
+        <label>Username</label>
+        <input
+          type='text'
+          name='username'
+          onChange={updateUsername}
+          value={username}
+        ></input>
+      </div>
+      <div>
+        <label>Email</label>
+        <input
+          type='text'
+          name='email'
+          onChange={updateEmail}
+          value={email}
+        ></input>
+      </div>
+      <div>
+        <label>Password</label>
+        <input
+          type='password'
+          name='password'
+          onChange={updatePassword}
+          value={password}
+        ></input>
+      </div>
+      <div>
+        <label>Confirm Password</label>
+        <input
+          type='password'
+          name='repeat_password'
+          onChange={updateRepeatPassword}
+          value={repeatPassword}
+          required={true}
+        ></input>
+      </div>
+      <button type='submit'>Sign Up</button>
+    </form>
   );
-}
+};
 
 export default SignUpPage;
