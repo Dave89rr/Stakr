bbeb84600c68a0fc480321609fc89b4be56e7092
Justin-Stockton <justinstockton67@gmail.com>
2022-07-21 16:07:18 -0400

    added the actions to stacks

--- frontend/src/store/stacks.js
+++ frontend/src/store/stacks.js
@@ -1,4 +1,15 @@
 // ==== Types ==== //
+
+const CREATE_STACK = "stack/CREATE_STACK";
+
+const GET_STACK = "stack/GET_STACK";
+
+const GET_STACKS = "stack/GET_STACKS";
+
+const UPDATE_STACK = "stack/UPDATE_STACK";
+
+const DELETE_STACK = "stack/DELETE_STACK";
+
 // ==== Actions ==== //
 // ==== Thunks ==== //
 // ==== Reducers ==== //
