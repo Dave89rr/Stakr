0b88beefbaad4c0f5eb31a396856e73d2a96d5e3
Justin-Stockton <justinstockton67@gmail.com>
2022-07-22 15:33:26 -0400

    added comment types

--- frontend/src/store/comments.js
+++ frontend/src/store/comments.js
@@ -1,4 +1,13 @@
 // ==== Types ==== //
+const CREATE_COMMENT = "card/CREATE_COMMENT";
+
+const GET_COMMENT = "card/GET_COMMENT";
+
+const GET_COMMENTS = "card/GET_COMMENTS";
+
+const UPDATE_COMMENT = "card/UPDATE_COMMENT";
+
+const DELETE_COMMENT = "card/DELETE_COMMENT";
 
 // ==== Actions ==== //
 
