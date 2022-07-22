c486ace2a0bb4d50a7bfd20ccba079e6cd668da2
Justin-Stockton <justinstockton67@gmail.com>
2022-07-22 12:44:38 -0400

    Set up actions for cards

--- frontend/src/store/cards.js
+++ frontend/src/store/cards.js
@@ -10,6 +10,34 @@ const DELETE_CARDS = "cards/GET_CARDS";
 
 // ==== Actions ==== //
 
+const actionCreateCard = (card) => {
+  return {
+    type: CREATE_CARDS,
+    card,
+  };
+};
+
+const actionGetCards = (stackId) => {
+  return {
+    type: CREATE_CARDS,
+    stackId,
+  };
+};
+
+const actionUpdateCard = (card) => {
+  return {
+    type: UPDATE_CARDS,
+    card,
+  };
+};
+
+const actionDeleteCard = (cardId) => {
+  return {
+    type: DELETE_CARDS,
+    cardId,
+  };
+};
+
 // ==== Thunks ==== //
 
 // ==== Reducer ==== //
