fcbcd3028b0778867c678d80dc0a400e1375c84d
David Rivera <daleriro@gmail.com>
2022-07-22 09:32:20 -0400

    Added logo to navBar, made it a link

--- frontend/src/components/Elements/Logo/Logo.js
+++ frontend/src/components/Elements/Logo/Logo.js
@@ -1,9 +1,25 @@
+import { Link } from 'react-router-dom';
+import classes from './Logo.module.css';
 function Logo({ user }) {
-  if (user) {
-    return <h1>About | Logo </h1>;
-  } else {
-    return <h1>Logo</h1>;
-  }
+  const logo = (
+    <img
+      className={classes.icons}
+      src="/media/icons/stakr-logo.svg"
+      alt="stakr logo"
+    />
+  );
+  const about = (
+    <img
+      className={classes.icons}
+      src="/media/icons/abouticon.svg"
+      alt="about icon"
+    />
+  );
+  return (
+    <div className={classes.container}>
+      {about} <Link to="/">{logo}</Link>
+    </div>
+  );
 }
 
 export default Logo;
