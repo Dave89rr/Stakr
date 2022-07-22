33ed4e1d494047a5732fcb31702d57ceae4b7f85
Justin-Stockton <justinstockton67@gmail.com>
2022-07-22 12:36:54 -0400

    Set up types for cards

--- frontend/src/store/cards.js
+++ frontend/src/store/cards.js
@@ -1,5 +1,13 @@
 // ==== Types ==== //
 
+const CREATE_CARDS = "cards/GET_CARDS";
+
+const GET_CARDS = "cards/GET_CARDS";
+
+const UPDATE_CARDS = "cards/GET_CARDS";
+
+const DELETE_CARDS = "cards/GET_CARDS";
+
 // ==== Actions ==== //
 
 // ==== Thunks ==== //
