a6fae084f0fa1db9cb004637126f6f2dfb8c7698
Justin-Stockton <justinstockton67@gmail.com>
2022-07-24 11:07:53 -0400

    setup for comment form done just need to code it out

---
+++ frontend/src/components/Forms/CommentForm/CommentForm.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { thunkCreateComment } from "../../../store/comments";
+
+function CommentForm() {
+  return <div>CommentForm</div>;
+}
+
+export default CommentForm;
