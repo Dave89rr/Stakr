fcf4820058417b0f24aff0f1260105423fe4eb9f
Brandon Flores <brandonflores647@gmail.com>
2022-07-23 23:00:33 -0400

    formatted board/stack html structure with proper overflow

--- frontend/src/components/Elements/Stack/Stack.js
+++ frontend/src/components/Elements/Stack/Stack.js
@@ -2,10 +2,15 @@ import classes from './Stack.module.css';
 
 const Stack = ({ data }) => {
     return (
+        <div className={classes.stackWrapper}>
         <div className={classes.stack}>
             <div className={classes.stackTitle}>
                 {data.name}
             </div>
+            <div className={classes.stackContent}>
+                {Array(21).fill('test').map(ele => <p>{ele}</p>)}
+            </div>
+        </div>
         </div>
     );
 }
