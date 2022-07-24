a442d2dd1989513be5eee645256a50122c1f1d0c
Justin-Stockton <justinstockton67@gmail.com>
2022-07-24 17:31:51 -0400

    mostly working form need to test

--- frontend/src/components/Forms/StacksForm/StacksForm.js
+++ frontend/src/components/Forms/StacksForm/StacksForm.js
@@ -1,19 +1,37 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { thunkCreateStack } from "../../../store/stacks";
+import { useParams } from "react-router-dom";
 
 function StacksForm() {
   const [name, setName] = useState("");
   const user = useSelector((state) => state.session.user);
-  const workspaces = useSelector((state) => state.workspaces);
+  const url = useParams();
+  const workspaceId = url.workspaceId;
+  const boardId = url.boardId;
   dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const stack = {
+      boardId,
+      workspaceId,
+      username: user.name,
+      name,
+    };
+    if (stack) {
+      dispatch(thunkCreateStack(stack));
+      setName("");
+    }
+  };
+
   return (
     <>
       <form onSubmit={handleSubmit}>
         <div>
-          {/* {validationErrors.map((error, ind) => (
-            <div key={ind}>{error}</div>
-          ))} */}
+          {validationErrors.map((error, i) => (
+            <div key={i}>{error}</div>
+          ))}
         </div>
         <div>
           <label htmlFor="name">Name</label>
