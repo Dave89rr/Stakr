911ee9385577dc564b3f6c39952a0896643fd704
Justin-Stockton <justinstockton67@gmail.com>
2022-07-25 23:32:47 -0400

    added a del button with the thunk to delete a stack

--- frontend/src/components/Elements/Stack/Stack.js
+++ frontend/src/components/Elements/Stack/Stack.js
@@ -1,76 +1,97 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { Droppable, Draggable } from 'react-beautiful-dnd';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Droppable, Draggable } from "react-beautiful-dnd";
 
-import Card from '../Card';
-import { thunkGetCards } from '../../../store/cards';
+import Card from "../Card";
+import { thunkGetCards } from "../../../store/cards";
+import { thunkDeleteStack } from "../../../store/stacks";
 
-import classes from './Stack.module.css';
+import classes from "./Stack.module.css";
 
 const Stack = ({ data, disabled, workspaces, cards, cardIds }) => {
-    const { workspaceId, boardId } = useParams();
-    const dispatch = useDispatch();
+  const { workspaceId, boardId } = useParams();
+  const dispatch = useDispatch();
 
-    // let cards;
-    // if (workspaces[workspaceId].cards) {
-    //     const allCards = Object.values(workspaces[workspaceId].cards);
-    //     cards = allCards.filter(ele => ele.stackId === data.id)
-    //         .sort((a, b) => a.position-b.position)
-    // }
-    let filteredCardIds;
-    if (cardIds) {
-        filteredCardIds = cardIds.filter(id => {
-            return cards[id].stackId === data.id
-        }).sort((a, b) => cards[a].position-cards[b].position);
-    }
+  // let cards;
+  // if (workspaces[workspaceId].cards) {
+  //     const allCards = Object.values(workspaces[workspaceId].cards);
+  //     cards = allCards.filter(ele => ele.stackId === data.id)
+  //         .sort((a, b) => a.position-b.position)
+  // }
+  let filteredCardIds;
+  if (cardIds) {
+    filteredCardIds = cardIds
+      .filter((id) => {
+        return cards[id].stackId === data.id;
+      })
+      .sort((a, b) => cards[a].position - cards[b].position);
+  }
 
-    useEffect(() => {
-        (async () => {
-          if (workspaces[workspaceId]) {
-            await dispatch(thunkGetCards(data.id, workspaceId));
-          }
-        })();
-      }, [dispatch, workspaces[workspaceId]]);
+  useEffect(() => {
+    (async () => {
+      if (workspaces[workspaceId]) {
+        await dispatch(thunkGetCards(data.id, workspaceId));
+      }
+    })();
+  }, [dispatch, workspaces[workspaceId]]);
 
-    return (
-        <Draggable
-            draggableId={`${data.id}`}
-            index={data.position}
-            key={data.id}
-            isDragDisabled={disabled}
+  // const handleDel = (e) => {
+  //   e.preventDefault();
+  //   console.log(data.id);
+  // };
+
+  return (
+    <Draggable
+      draggableId={`${data.id}`}
+      index={data.position}
+      key={data.id}
+      isDragDisabled={disabled}
+    >
+      {(provided) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          className={classes.stackWrapper}
         >
-        {(provided) => (
-            <div
-                ref={provided.innerRef}
-                {...provided.draggableProps}
-                className={classes.stackWrapper}
+          <div className={classes.stack}>
+            <div className={classes.stackTitle} {...provided.dragHandleProps}>
+              {data.name}
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(thunkDeleteStack(data.id));
+                }}
+              >
+                Del
+              </button>
+            </div>
+            <Droppable
+              droppableId={`drop${data.id}`}
+              direction="vertical"
+              type="row"
             >
-            <div className={classes.stack}>
-                <div className={classes.stackTitle} {...provided.dragHandleProps}>
-                    {data.name}
+              {(provided) => (
+                <div
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  className={classes.stackContent}
+                >
+                  {cards
+                    ? filteredCardIds.map((ele) => {
+                        return <Card data={cards[ele]} key={ele} />;
+                      })
+                    : null}
+                  {provided.placeholder}
+                  <p>+ New Card</p>
                 </div>
-                <Droppable droppableId={`drop${data.id}`} direction='vertical' type='row'>
-                {(provided) => (
-                    <div
-                        {...provided.droppableProps}
-                        ref={provided.innerRef}
-                        className={classes.stackContent}
-                    >
-                        {cards ? filteredCardIds.map((ele) => {
-                            return <Card data={cards[ele]} key={ele}/>
-                        })
-                        : null}
-                        {provided.placeholder}
-                        <p>+ New Card</p>
-                    </div>
-                )}
-                </Droppable>
-            </div>
-            </div>
-        )}
-        </Draggable>
-    );
-}
+              )}
+            </Droppable>
+          </div>
+        </div>
+      )}
+    </Draggable>
+  );
+};
 
-export default Stack
+export default Stack;
