229f417a78456a8915a62d1e679ad33fea6148a6
Brandon Flores <brandonflores647@gmail.com>
2022-07-28 01:34:44 -0400

    added custom colors for boardCards

--- frontend/src/components/Elements/BoardCard/BoardCard.js
+++ frontend/src/components/Elements/BoardCard/BoardCard.js
@@ -1,12 +1,49 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 
 import classes from './BoardCard.module.css';
 
 function BoardCard({ data }) {
+
+  const [bgc, setBgc] = useState('#ffffff');
+
+  useEffect(() => {
+    //handle color
+    switch(data.color) {
+      case 'White':
+        setBgc(classes.white);
+        break;
+      case 'Red':
+        setBgc(classes.red);
+        break;
+      case 'Orange':
+        setBgc(classes.orange);
+        break;
+      case 'Blue':
+        setBgc(classes.blue);
+        break;
+      case 'Yellow':
+        setBgc(classes.yellow);
+        break;
+      case 'Green':
+        setBgc(classes.green);
+        break;
+      case 'Purple':
+        setBgc(classes.purple);
+        break;
+      case 'Pink':
+        setBgc(classes.pink);
+        break;
+      case 'Grey':
+        setBgc(classes.grey);
+        break;
+    }
+  }, []);
+
   return (
-    <Link to={`/b/${data.workspaceId}/${data.id}/${data.name}`}>
-      <div className={classes.boardcard} >
-        <span>{data.name}</span>
+    <Link to={`/b/${data.workspaceId}/${data.id}/${data.name}`} style={{textDecoration:'none'}}>
+      <div className={`${classes.boardcard} ${bgc}`} >
+        {data.name}
       </div>
     </Link>
   );
