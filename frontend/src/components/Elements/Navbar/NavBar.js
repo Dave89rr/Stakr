632372fdb6691e3a80087fdf44ad31e477d900ef
Brandon Flores <brandonflores647@gmail.com>
2022-07-28 18:44:12 -0400

    added demo login button

--- frontend/src/components/Elements/Navbar/NavBar.js
+++ frontend/src/components/Elements/Navbar/NavBar.js
@@ -2,20 +2,34 @@ import classes from './NavBar.module.css';
 import Logo from '../Logo';
 import LogoutButton from '../LogoutButton';
 import Menu from '../Menu';
+import { useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { login } from '../../../store/session';
 
 function NavBar({ user }) {
+  const dispatch = useDispatch();
+
+  const handleDemoLogin = async () => {
+    await dispatch(login('demo@aa.io', 'password'));
+  }
+
   return (
     <nav>
       <div className={classes.leftContainer}>
         <Logo user={user} />
-        <Menu />
+        {user?<Menu />:null}
       </div>
       <div className={classes.userInteractionsContainer}>
         {user ? (
           <LogoutButton />
         ) : (
           <>
+            <div
+              onClick={handleDemoLogin}
+              className={classes.demoLoginBtn}
+            >
+              Demo
+            </div>
             <NavLink to="/login">Login</NavLink>
             <NavLink to="/signup">Signup</NavLink>
           </>
