757150d0142140a492a8e035dd64b8ccf3d427ff
Brandon Flores <brandonflores647@gmail.com>
2022-07-28 18:16:19 -0400

    styled login/signup buttons

--- frontend/src/components/Elements/LogoutButton/LogoutButton.js
+++ frontend/src/components/Elements/LogoutButton/LogoutButton.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { logout } from "../../../store/session";
 import { thunkLogoutWorkspace } from "../../../store/workspaces";
 
 const LogoutButton = () => {
+  const history = useHistory();
   const dispatch = useDispatch();
   const onLogout = async (e) => {
     await dispatch(thunkLogoutWorkspace());
     await dispatch(logout());
+    history.push('/login');
   };
 
   return <button onClick={onLogout}>Logout</button>;
