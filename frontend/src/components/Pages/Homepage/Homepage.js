e49597ab03e2cb94ba558b95122734cd3c48ee18
Justin-Stockton <justinstockton67@gmail.com>
2022-07-28 22:04:47 -0400

    top is mostly styled

--- frontend/src/components/Pages/Homepage/Homepage.js
+++ frontend/src/components/Pages/Homepage/Homepage.js
@@ -4,11 +4,17 @@ function HomePage() {
   return (
     <div className={classes.mainContainer}>
       <div className={classes.gradient}>
-        <h1>Howdy</h1>
-      </div>
-      <div></div>
-      <div>
-        <h1>Howdy</h1>
+        <div className={classes.topContainer}>
+          <h1 className={classes.header}>
+            Stakr helps teams move work forward.
+          </h1>
+          <p className={classes.subHeaderText}>
+            Collaborate, manage projects, and reach new productivity peaks. From
+            high rises to the home office, the way your team works is
+            unique—accomplish it all with Stakr.
+          </p>
+        </div>
+        <div className={classes.line}></div>
       </div>
     </div>
   );
