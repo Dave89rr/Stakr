214c652e5b5b87b15869f5c8964dd789dce4fa4d
Dave89rr <daleriro@gmail.com>
2022-07-30 20:07:15 -0400

    udpated createmenu

--- frontend/src/components/Elements/CreateMenu/CreateMenu.js
+++ frontend/src/components/Elements/CreateMenu/CreateMenu.js
@@ -5,15 +5,63 @@ import classes from './CreateMenu.module.css';
 function CreateMenu({ setToggleView }) {
   const [showMenu, setShowMenu] = useState(true);
   const [showWSF, setShowWSF] = useState(false);
+  const [showBF, setShowBF] = useState(false);
+
+  const boardIcon = (
+    <img
+      className={classes.stakrIcon}
+      src="/media/icons/boardicon.svg"
+      alt="stakr icon"
+    />
+  );
+  const workspaceIcon = (
+    <img
+      className={classes.stakrIcon}
+      src="/media/icons/workspaceicon.svg"
+      alt="stakr icon"
+    />
+  );
+  const toggleCreateBoard = () => {
+    //
+  };
+  const toggleCreateWorkspace = () => {
+    setShowWSF(true);
+    setShowMenu(false);
+  };
+  const menuContent = (
+    <div className={classes.contentContainer}>
+      <div className={classes.createAction} onClick={toggleCreateBoard}>
+        <span>
+          {boardIcon} <span>Create Board</span>
+        </span>
+        <div className={classes.createDescription}>
+          <p>
+            A board is made up of cards ordered on stacks. Use it to manage
+            projects, track information, or organize anything.
+          </p>
+        </div>
+      </div>
+      <div className={classes.createAction} onClick={toggleCreateWorkspace}>
+        <span>
+          {workspaceIcon} <span>Create Workspace</span>
+        </span>
+        <div className={classes.createDescription}>
+          <p>
+            A Workspace is a group of boards. Use it to organize your company,
+            side hustle, family, or friends.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
   return (
     <div className={classes.container}>
       <div className={classes.createTitle}>Create</div>
       <div className={classes.line}></div>
-      {showMenu ? <span>Create Workspace</span> : null}
-      <WorkspacesForm setToggleView={setToggleView} setShowWSF={setShowWSF} />
-      {/* {showWSF ? (
+      {showMenu ? menuContent : null}
+      {showWSF ? (
         <WorkspacesForm setToggleView={setToggleView} setShowWSF={setShowWSF} />
-      ) : null} */}
+      ) : null}
     </div>
   );
 }
